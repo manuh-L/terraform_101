665b34e7436451403f17fb5198aaeb2d96ba187e
manuh-L <manuel.nhiuana@gmail.com>
2020-08-04 00:01:28 +0200

    ansible d inventory

---
+++ terraform_ansible/main.tf
@@ -0,0 +1,118 @@
+##################################################################################
+# VARIABLES
+##################################################################################
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "private_key_path" {}
+variable "key_name" {}
+variable "region" {
+  default = "af-south-1"
+}
+
+##################################################################################
+# PROVIDERS
+##################################################################################
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.region
+}
+
+##################################################################################
+# DATA
+##################################################################################
+
+data "aws_ami" "aws-linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+   filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  
+
+}
+
+##################################################################################
+# RESOURCES
+##################################################################################
+
+#This uses the default VPC.  It WILL NOT delete it on destroy.
+resource "aws_default_vpc" "default" {
+
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "SSH_HTTP_DEMO"
+  description = "Allow ports 22 & 80"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "apache_terraform" {
+  ami                    = data.aws_ami.aws-linux.id
+  instance_type          = "t3.micro"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y"
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.private_key_path)
+
+  }
+  
+  provisioner "local-exec" {
+    command = "ansible ${self.public_ip} -m ping --private-key ${var.private_key_path} "
+  }
+
+
+}
+
+##################################################################################
+# OUTPUT
+##################################################################################
+
+output "aws_instance_public_dns" {
+  value = aws_instance.apache_terraform.public_dns
+}
---
+++ terraform_ansible/terraform.tfvars
@@ -0,0 +1,7 @@
+aws_access_key = "AKIA2BIHHWQRL5B3OXEO"
+
+aws_secret_key = "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"
+
+key_name = "PrivateSvr"
+
+private_key_path = "/home/admin/Documents/PrivateSvr.pem"
