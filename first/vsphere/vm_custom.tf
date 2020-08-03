7aa6ce1af2885a434295fc051513be734c39e550
Manuel Nhiuana <manuel.nhiuana@gmail.com>
2020-08-03 18:47:19 +0200

    editado vars

--- first/vsphere/vm_custom.tf
+++ first/vsphere/vm_custom.tf
@@ -1,3 +1,6 @@
+##################################################################################
+# VARIABLES
+##################################################################################
 variable "vsphere_user" {}
 variable "vsphere_password" {}
 variable "vsphere_server" {}
@@ -21,7 +24,7 @@ provider "vsphere" {
   password       = var.vsphere_password
   vsphere_server = var.vsphere_server
 
-  # If you have a self-signed cert
+ 
   allow_unverified_ssl = true
 }
 
